9206911cd5c0cf40309535382f8806210efe7b4e
Vipin Kumar Gupta <vipinkumargupta@gmail.com>
2020-12-29 19:56:58 +0530

    Update example.rb

--- sample_profile/controls/example.rb
+++ sample_profile/controls/example.rb
@@ -5,15 +5,10 @@
 title 'Sample Section'
 
 # Plural resources can be inspected to check for specific resource details
-control 'aws-vpcs-multi-region-status-check' do                             # A unique ID for this control.
-  impact 1.0                                                                # The criticality, if this control fails.
-  title 'Check AWS VPCs in all regions have status "available"'             # A human-readable title.
-  aws_regions.region_names.each do |region|                                 # Loop over all available AWS regions
-    aws_vpcs(aws_region: region).vpc_ids.each do |vpc|                      # Find all VPCs in a single AWS region
-      describe aws_vpc(aws_region: region, vpc_id: vpc) do                  # The test itself.
-        it { should exist }                                                 # Confirms AWS VPC exists
-        it { should be_available }                                          # Confirms AWS VPC has status "available"
-      end
-    end
-  end
+describe aws_ec2_instance('i-01a2349e94458a507') do
+  it { should exist }
+end
+
+describe aws_ec2_instance(name: 'my-instance') do
+  it { should exist }
 end
